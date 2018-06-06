70d98068704916526dd225c130f9fefadf80d54a
Ethan <erogers999@gmail.com>
2018-06-06 13:49:36 -0400

    update

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 var path = require('path');
 var htmlRoutes = require('./app/routes/htmlRoutes.js');
-
+require('dotenv').config();
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -11,11 +12,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 
  
 htmlRoutes(app); 
 
+app.post('/send', function(req, res, next) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.USERNAME,
+      pass: process.env.PASSWORD
+    }
+  })
+  const mailOptions = {
+    from: `${req.body.email}`,
+    to: 'erogers9909@gmail.com',
+    subject: `${req.body.name}`,
+    text: `${req.body.message}`
+  }
+  transporter.sendMail(mailOptions, function(err, res) {
+    if (err) {
+      console.error('there was an error: ', err);
+    } else {
+      console.log('here is the res: ', res)
+    }
+  })
+})
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
